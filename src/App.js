916df761ada81869a47954b7363c595d3eb2d79c
Eden <Edenlevy12@gmail.com>
2024-02-01 19:47:54 +0200

    branch test #2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React, good good good
+          Learn React, good good good good!
         </a>
       </header>
     </div>
