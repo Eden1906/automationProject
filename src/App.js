3a0704c1861d008f1228edb1662ee4e31389f0cf
Eden <Edenlevy12@gmail.com>
2024-02-02 16:17:17 +0200

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css';
+import NavBar from './components/Navbar'
+import Products from './components/Products'
+import Home from './components/Home'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React, good good good
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div classname = "App">
+        <NavBar/>
+        <Routes>
+          <Route path="/Products" element={<Products />} />
+          <Route path="/Home" element= {<Home />} />
+        </Routes>
+      </div>
+    </Router>
+    
+  )
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../App.css'
+
+const NavBar = () => {
+    
+    return (
+        <nav classname = "navbar">
+        <ul>
+            <li><a href="/Products">products</a></li>
+            <li><a href="/Home">home</a></li>
+        </ul>
+        </nav>
+    )
+}
+
+export default NavBar
