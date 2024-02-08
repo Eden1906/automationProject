76e68b134898c876c19910eefe2308e7458880fc
Eden <Edenlevy12@gmail.com>
2024-02-08 17:10:39 +0200

    added order option

--- src/components/ProductsUpload.js
+++ src/components/ProductsUpload.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
-import {collection, getDocs, deleteDoc, doc } from "firebase/firestore";
+import {collection, getDocs, deleteDoc, doc, addDoc } from "firebase/firestore";
 import { txtDB } from '../firebaseConfig';
+import Order from './Order'
 
 
 const ProductsUpload = () => {
     const [products, setProducts] = useState([])
+    const [cart, setCart] = useState([])
+    const [showCartModal, setShowCartModal] = useState(false)
 
     useEffect (() => {
         const fetchProducts = async () => {
@@ -38,15 +41,59 @@ const ProductsUpload = () => {
     }
 }
 
+const handleAddToCart = (productId) => {
+    const productToAdd = products.find(product => product.id === productId)
+    setCart((prevCart) => [...prevCart, productToAdd])
+}
+
+const handleClearCart = () => {
+    setCart([])
+}
+
+const toggleCartModal = () => {
+    setShowCartModal(!showCartModal)
+}
+
+const handlePlaceOrder = async (orderDetails) => {
+    try {
+        const orderDocRef = await addDoc(collection(txtDB, "orders"), {
+            products: cart,
+            name: orderDetails.name,
+            address: orderDetails.address,
+        })
+        console.log(orderDocRef.id)
+        setCart([])
+        setShowCartModal(false)
+    } catch (error) {
+        console.error("error", error)
+    }
+}
+
     return (
         <div className="container">     
+            <button onClick={toggleCartModal}>shopping cart</button>
+            {showCartModal && (
+                <div className="cart">
+                <ul>
+                    {cart.map((cartItem) => (
+                        <li key={cartItem.id}>
+                            <p> {cartItem.name} - {cartItem.price}₪</p>
+                        </li>
+                    ))}
+                </ul>
+                <button onClick={handleClearCart}>clear cart</button>
+                <Order onPlaceOrder={handlePlaceOrder} />
+            </div>
+            )}     
             <ul className="product-list">
                 {products.map((product) => (
                     <li key={product.id}>
                         <h3> {product.name} </h3>
                         <p> price: {product.price}₪</p>
                         <img src = {product.imageUrl} alt = {product.name} className = "product-image" />
-                        <button onClick={() => handleDelete(product.id)}>delete</button>
+                        <button onClick={() => handleDelete(product.id)}> delete </button>
+                        <button onClick={() => handleAddToCart(product.id)}> add to cart </button>
+                        
                     </li>
                 ))}
             </ul>
