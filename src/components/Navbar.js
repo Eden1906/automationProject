09a1977b36fcbc0efa17fd101e6645c8f95fde1e
Sasha191312 <alex.perednya20@gmail.com>
2024-02-10 14:52:32 +0200

    editing navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,42 +1,57 @@
-import '../App.css'
+import { IDLE_BLOCKER } from "@remix-run/router";
+import "../App.css";
+import logo from "./assets/logo.png";
+import cart_icon from "./assets/cart-icon.png";
+import "./Navbar/Navbar.css";
+import { useState } from "react";
 
 const NavBar = () => {
-
-    const handlePasswordSubmit = () => {
-        const password = prompt('enter password')
-        if (password !== null) {
-            if (password === '1234') {
-                window.location.href = '/Seller'
-            }
-            else {
-                alert('incorrect password')
-            }
-        }
+  const handlePasswordSubmit = () => {
+    const password = prompt("enter password");
+    if (password !== null) {
+      if (password === "1234") {
+        window.location.href = "/Seller";
+      } else {
+        alert("incorrect password");
+      }
     }
+  };
 
-    return (
-        <nav className = "navbar">
-        <ul>
-            <li><a href="/Products">products</a></li>
-            <li>
-                <button 
-                style={{
-                backgroundColor: 'transparent',
-                color: 'black',
-                fontWeight: 'bold',
-                border: 'none',
-                padding: 0,
-                textAlign: 'left',
-                textDecoration: 'underline',
-                display: 'inline',
-                fontSize: '16px',
-                cursor: 'pointer',
-            }}
-                onClick={handlePasswordSubmit}> sign in as seller </button>
-            </li>
-        </ul>
-        </nav>
-    )
-}
+  return (
+    <div className="navbar">
+      <div className="nav-logo">
+        <img className="logo-img" src={logo} alt="" />
+      </div>
+      <ul className="nav-menu">
+        <li>
+          <a href="/Products">Products</a>
+        </li>
+        <li>
+          <a href="/seller" onClick={handlePasswordSubmit}>
+            Sign in as Seller
+          </a>
+        </li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img className="cart-img" src={cart_icon} alt="" />
+        <div className="nav-cart-count">0</div>
+      </div>
+    </div>
 
-export default NavBar
+    // <nav className="navbar">
+    //   <ul>
+    //     <li>
+    //       <a href="/Products">products</a>
+    //     </li>
+    //     <li>
+    //       <a href="/seller" onClick={handlePasswordSubmit}>
+    //         sign in as seller
+    //       </a>
+    //     </li>
+    //   </ul>
+    // </nav>
+  );
+};
+
+export default NavBar;
